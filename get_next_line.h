24e8cc4b75d7f1d42468e10029ea3471f53f39a5
taisei-13046 <78260526+taisei-13046@users.noreply.github.com>
2021-04-27 23:45:11 +0900

    first

---
+++ get_next_line.h
@@ -0,0 +1,11 @@
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <string.h>
+# include <fcntl.h>
+# include <unistd.h>
+# include <stdlib.h>
+
+int	get_next_line(int fd, char **line);
+
+#endif
