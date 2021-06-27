7e28db63ed9e966479f7236a6980bdbf435c1846
taisei-13046 <78260526+taisei-13046@users.noreply.github.com>
2021-06-28 00:44:17 +0900

    complete

--- get_next_line.h
+++ get_next_line.h
@@ -6,6 +6,10 @@
 # include <unistd.h>
 # include <stdlib.h>
 
-int	get_next_line(int fd, char **line);
+int		get_next_line(int fd, char **line);
+size_t	ft_strlen(const char *str);
+char	*ft_strdup(char *str);
+int		ft_strchr(char *save);
+char	*ft_strjoin(char *save, char *buf);
 
 #endif
